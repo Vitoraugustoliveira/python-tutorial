1149cac753ecce6a26b49487bd7a1fcaecb727c6
Arthur Barbosa <arthuralves187@gmail.com>
2021-06-25 16:42:21 -0300

    get biggest number

---
+++ get_biggest.py
@@ -0,0 +1,7 @@
+numero1 = float(input("Digite o numero 1\n"))
+numero2 = float(input("Digite o numero 2\n"))
+numero3 = float(input("Digite o numero 3\n"))
+
+lista_entrada = [numero1, numero2, numero3]
+lista_entrada.sort()
+print("O maior numero é: ", lista_entrada[-1])
