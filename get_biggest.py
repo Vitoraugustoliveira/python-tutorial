065b8d9d8a5088dfdb2abf3310f5041b95db8378
arthur-barbosa <arthuralves187@gmail.com>
2021-06-25 17:36:20 -0300

    Terceira lista de exercicio

--- get_biggest.py
+++ get_biggest.py
@@ -1,7 +1,25 @@
-numero1 = float(input("Digite o numero 1\n"))
-numero2 = float(input("Digite o numero 2\n"))
-numero3 = float(input("Digite o numero 3\n"))
+# numero1 = float(input("Digite o numero 1\n"))
+# numero2 = float(input("Digite o numero 2\n"))
+# numero3 = float(input("Digite o numero 3\n"))
 
+
+numero1 = 40
+numero2 = 30
+numero3 = 50
 lista_entrada = [numero1, numero2, numero3]
 lista_entrada.sort()
 print("O maior numero é: ", lista_entrada[-1])
+print("O menor numero é: ", lista_entrada[0])
+
+
+#imprimindo em ordem descrescente
+
+lista= ["Z", "A", "T"]
+lista.sort(reverse = True)
+print(lista)
+
+lista_entrada.sort(reverse = True)
+for item in lista_entrada:
+	print(item)
+
+
