c4150442f5110d909e6d4f246e3fba07d35b0b98
arthur-barbosa <arthuralves187@gmail.com>
2021-06-29 16:46:42 -0300

    Code to validate prime number

---
+++ prime_number_validation.py
@@ -0,0 +1,14 @@
+number = int(input("Digite um numero para validar se ele é primo\n"))
+
+primo = True
+for i in range(1, number+1):
+	print(i)
+	if i == 1 or i == number:
+		continue
+	if number%i == 0:
+		print("Número {} não é primo".format(number))
+		primo = False
+		break
+
+if primo == True:
+	print("Número {} é primo".format(number))
